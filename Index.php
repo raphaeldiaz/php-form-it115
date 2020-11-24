2b3712fd9f7156a8c8e47b4fa0cf2e870a61d89e
raphaeldiaz <72459750+raphaeldiaz@users.noreply.github.com>
2020-11-24 09:15:30 -0800

    Update Index.php

--- Index.php
+++ Index.php
@@ -1,4 +1,20 @@
 <?php
-Echo '<p>This is PHP! </p>';
+//Echo '<p>This is PHP! </p>';
+
+if (isset($GET["FirstName"])){ //show feedback
+  // echo $_GET["FirstName"];
+  
+  echo '<pre>';
+  var_dump($_GET);
+  echo '</pre>';
+  
+  
+}else { // show form
+  echo '
+  <form action="">
+  First Name: <input type="text" name="FirstName" /><br />
+  <input type="submit" />
+  </form>
+  ';
+}
 ?>
-<p>This is HTML</p>
